e6fe35990389d599bb5e521ce572ff07312dd93f
cbcote <christopher.cote88@gmail.com>
2023-11-21 20:47:40 -0600

    Switched to pdfreader

--- src/data/pdf_extraction.py
+++ src/data/pdf_extraction.py
@@ -1,15 +1,15 @@
-import PyPDF2
+import pdfreader
+from pdfreader import PDFDocument, SimplePDFViewer
 
 class PDFExtractor:
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
 
     def extract_text(self):
-        pdf_file_obj = open(self.pdf_path, 'rb')
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-        text = ''
-        for page_num in range(pdf_reader.numPages):
-            page_obj = pdf_reader.getPage(page_num)
-            text += page_obj.extractText()
-        pdf_file_obj.close()
+        with open(self.pdf_path, "rb") as fd:
+            viewer = SimplePDFViewer(fd)
+            text = ''
+            for canvas in viewer:
+                viewer.render()
+                text += ' '.join(viewer.canvas.strings)
         return text
