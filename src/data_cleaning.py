18d8bddf22f0bd52be02dcde38b2392ad1cb0623
cbcote <christopher.cote88@gmail.com>
2023-12-23 12:40:03 -0600

    Refactor data cleaning methods

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,27 +2,62 @@ import re
 
 
 class DataCleaner:
-    def __init__(self, raw_text):
+    def __init__(self, raw_text: str) -> None:
         self.raw_text = raw_text
 
-    def normalize_whitespace(self):
+    def normalize_whitespace(self) -> str:
+        """
+        Normalizes the whitespace in the text.
+        
+        Returns:
+            str: The cleaned text.
+        """
         cleaned_text = ' '.join(self.raw_text.split())
         return cleaned_text
     
-    def remove_headers_footers(self):
-        # Assuming headers and footers follow a pattern that can be identified by a regex
+    def remove_headers_footers(self) -> None:
+        """
+        Removes the headers and footers from the text.
+        
+        Returns:
+            str: The cleaned text.
+        """
         self.raw_text = re.sub(r'Header or fotter pattern', '', self.raw_text)
 
-    def remove_unwanted_characters(self):
+    def remove_unwanted_characters(self) -> None:
+        """
+        Removes unwanted characters from the text.
+        
+        Returns:
+            str: The cleaned text.
+        """
         self.raw_text = re.sub(r'[^\x00-\x7F]+', '', self.raw_text)  # Removes non-ASCII characters
 
-    def replace_newlines(self):
+    def replace_newlines(self) -> None:
+        """
+        Replaces newlines with spaces.
+        
+        Returns:
+            str: The cleaned text.
+        """
         self.raw_text = self.raw_text.replace('\n', ' ')
 
-    def collapse_punctuation(self):
+    def collapse_punctuation(self) -> None:
+        """
+        Collapses punctuation in the text.
+        
+        Returns:
+            str: The cleaned text.
+        """
         self.raw_text = re.sub(r'[\.\,\'"!?;:-]{2,}', lambda x: x.group(0)[0], self.raw_text)
 
-    def clean_text(self):
+    def clean_text(self) -> str:
+        """
+        Cleans the text.
+        
+        Returns:
+            str: The cleaned text.
+        """
         self.normalize_whitespace()
         self.remove_headers_footers()
         self.remove_unwanted_characters()
